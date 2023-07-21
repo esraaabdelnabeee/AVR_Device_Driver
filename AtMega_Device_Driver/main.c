12a8bb2792bd0f12a2e5e6ce039fc4eabf9530ca
esraaabdelnabeee <140099987+esraaabdelnabeee@users.noreply.github.com>
2023-07-21 21:34:58 +0300

    Add files via upload

---
+++ AtMega_Device_Driver/main.c
@@ -0,0 +1,19 @@
+/*
+ * AtMega_Device_Driver.c
+ *
+ * Created: 23/06/22 9:28:02 AM
+ * Author : ESRAA_ABDELANBE
+ */ 
+#define F_CPU 16000000UL
+#include <util/delay.h>
+
+int main(void)
+{
+    /* Replace with your application code */
+
+    while (1) 
+    {
+					
+    }
+}
+
